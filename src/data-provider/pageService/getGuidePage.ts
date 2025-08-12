566f5babef06c38d9091baad5272c0483f59c644
Jose <jose.gr98@yahoo.com>
2025-08-12 16:04:26 -0300

    -Fixed build

--- src/data-provider/pageService/getGuidePage.ts
+++ src/data-provider/pageService/getGuidePage.ts
@@ -22,8 +22,6 @@ export const getGuidePage = async ({ query }: Props): Promise<UiPage> => {
     fetchPolicy,
   });
 
-  console.log("data", data);
-
   const components = data.guide_All.items[0].components.items;
   const title = data.guide_All.items[0].title;
 
@@ -32,6 +30,8 @@ export const getGuidePage = async ({ query }: Props): Promise<UiPage> => {
     title: data.guide_All.items[0].title,
     publishedDate: data.guide_All.items[0].publishedDate,
     description: data.guide_All.items[0].description,
+    __typename: data.guide_All.items[0].__typename,
+    slug: data.guide_All.items[0].slug,
   } as const satisfies Guide;
 
   const mappedComponents = mapComponents({ components, pageData });
