4df18b8c2a2e4b021162500387e9f6a6fd5d7d4a
Jose <jose.gr98@yahoo.com>
2025-09-10 10:35:01 -0300

    -testing if cachce persists accross builds

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,8 @@ export const metadata = {
 export default async function Home() {
   const data = await getHomePage();
 
+  console.log("TESTING PRESISTANT CACHE");
+
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center  p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
